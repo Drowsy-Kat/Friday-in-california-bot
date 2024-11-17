bc72332a11e02e0e90c3ec975802597e20dba32c
Drowsy-Kat <khizer78901@gmail.com>
2024-11-17 00:07:22 +0000

    ok i can leave this now

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import { CronJob } from 'cron'; // Changed require to import
-import { IgApiClient } from 'instagram-private-api';
-//import { get } from 'request-promise';
-import express from 'express';
-import { promisify } from 'util';
-import { readFile } from 'fs'; 
+require("dotenv").config();
+const CronJob = require("cron").CronJob;
+const { IgApiClient } = require('instagram-private-api');
+const { get } = require('request-promise');
+const express = require('express');  // Change to require
+const { promisify } = require('util');
+const { readFile } = require('fs');
 
 const readFileAsync = promisify(readFile);
 const app = express();
@@ -22,12 +20,6 @@ const postToInsta = async () => {
     const ig = new IgApiClient();
     ig.state.generateDevice(process.env.IG_USERNAME);
     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-
-    // const videoBuffer = await get({
-    //     url: 'https://drowsykat.dev/Share/videos/friday.mp4',
-    //     encoding: null,
-    // })
-    // console.log("Fetched video buffer:", videoBuffer);
 
     const videoPath = './friday.mp4'
     const coverPath = './friday.jpg'
@@ -45,9 +37,9 @@ const postToInsta = async () => {
 
 
 
-const cronInsta = new CronJob("0 14 * * FRI", async () => {
+const cronInsta = new CronJob("* 14 * * FRI", async () => {
     postToInsta();
 })
 
-postToInsta();
+//postToInsta();
 cronInsta.start();
